1f608f10483180f738931b4eadd50c8ad0981a2a
deepcode978 <aakash978h@gmail.com>
2021-12-10 11:49:27 +1100

    Loading state progress bar

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Router from 'next/router';
+import nProgress from 'nprogress';
+import '../styles/globals.css';
+import '../styles/nprogress.css';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
